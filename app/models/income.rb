c2bc28820f8606869b90aa9912b801f07b7411b2
narasimha <narasimha@codebrahma.com>
2018-10-11 22:26:37 +0530

    income validation fix

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,9 +2,13 @@ class Income < ApplicationRecord
 	validates :recieved_on, :description, :amount, presence: true
 	validate :check_recieved_on
 
+	scope :order_by_recieved_on, -> { order("recieved_on ASC") }
+
+	default_scope { order_by_recieved_on }
+
 	def check_recieved_on
 		return true if recieved_on.blank?
-		return true unless (Date.parse(recieved_on.to_s) <= Date.today)
+		return true if (Date.parse(recieved_on.to_s) <= Date.today)
 		errors.add(:recieved_on, 'Should not be in future.')
 	end
 end
