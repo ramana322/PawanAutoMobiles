7256adf2b2c59a5e57884aaa9073cc4464106bdb
narasimha <narasimha@codebrahma.com>
2018-10-12 18:01:59 +0530

    Added delivery index page

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -5,4 +5,14 @@ class Delivery < ApplicationRecord
 	validates :customer, presence: true
 
 	accepts_nested_attributes_for :customer
+	
+	delegate :name,
+			 to: :customer,
+			 prefix: true,
+			 allow_nil: false
+
+	delegate :mobile_number,
+			 to: :customer,
+			 prefix: true,
+			 allow_nil: false
 end
