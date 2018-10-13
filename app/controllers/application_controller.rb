961d8b239ba0b42c4c88ec17d88a65c38035594d
narasimha <narasimha@codebrahma.com>
2018-10-13 12:45:25 +0530

    devise views added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  layout "application"
+  before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+	def layout_by_resource
+    if devise_controller? || controller_name == "sessions" || controller_name == "registrations"
+      "auth"
+    else
+      "application"
+    end
+  end
 end
