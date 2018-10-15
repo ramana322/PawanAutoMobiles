f9c965329a81d89147d5a38df75d78f554246d9a
narasimha <narasimha@codebrahma.com>
2018-10-15 21:55:27 +0530

    expenditure crud added

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -5,7 +5,9 @@ class ExpendituresController < ApplicationController
 	end
 
 	def index
-		@expenditures = Expenditure.all
+    @expenditures = Expenditure.all.group_by do |expenditure|
+      expenditure.spent_on.beginning_of_month 
+    end.sort.reverse.to_h
 	end
 
 	def create
@@ -13,7 +15,7 @@ class ExpendituresController < ApplicationController
 			
 		if @expenditure.save
 			flash[:notice] = "Expenditure was successfully created"
-			redirect_to expenditure_path(@expenditure)	 
+			redirect_to expenditures_path
 		else
 			render 'new'
 		end
@@ -22,27 +24,32 @@ class ExpendituresController < ApplicationController
   def show
  		@expenditure = Expenditure.find(params[:id])
  	end
+  
   def edit
-  @expenditure = Expenditure.find(params[:id])
+    @expenditure = Expenditure.find(params[:id])
   end
+
   def update
   	@expenditure = Expenditure.find(params[:id])
+
   	if @expenditure.update(expenditure_params)
   		flash[:notice] = "Expenditure was successfully Updated"
-  		redirect_to expenditure_path(@expenditure)	
+  		redirect_to expenditures_path	
   	else 
   		render'edit'
   	end
   end
+
   def destroy
   	@expenditure = Expenditure.find(params[:id])
   	@expenditure.destroy
   	flash[:notice] = "Expenditure was successfully Deleted"
   	redirect_to expenditures_path
   end
+
 	private
 
   def expenditure_params
-    params.require(:expenditure).permit(:spent_on, :paid_to, :description, :amount)
+    params.require(:expenditure).permit(:spent_on, :description, :amount)
 	end
 end
